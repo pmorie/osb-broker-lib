49963b0ab8ea8e163983051aae0c4846c6c8ed42
Shawn Hurley <shawn.hurley21@gmail.com>
2018-03-10 19:18:22 -0500

    set new types in interface
    * removing new field for follow on PR.

--- pkg/broker/types.go
+++ pkg/broker/types.go
@@ -10,10 +10,6 @@ type CatalogResponse struct {
 // ProvisionResponse is sent as the response to a provision call.
 type ProvisionResponse struct {
 	osb.ProvisionResponse
-
-	// AlreadyReceived  is used to determine if the request was a duplicate
-	// or not. should not be sent back in the respone.
-	AlreadyReceived bool `json:"-"`
 }
 
 // UpdateInstanceResponse is sent as the response to a update call.
@@ -34,11 +30,6 @@ type LastOperationResponse struct {
 // BindResponse is sent as the response to a bind call.
 type BindResponse struct {
 	osb.BindResponse
-
-	// AlreadyReceived  is used to determine if the request was a duplicate
-	// or not. should not be sent back in the respone. This is needed for
-	// async bind.
-	AlreadyReceived bool `json:"-"`
 }
 
 // UnbindResponse is sent as the response to a bind call.
