18f3aa144f1880846501710964f645fdbcc534b5
Jay McConnell <jmmccon@amazon.com>
2018-08-14 15:38:14 -0700

    add basic auth support

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -7,13 +7,30 @@ import (
 	"net/http"
 	"time"
 
+	auth "github.com/abbot/go-http-auth"
 	"github.com/golang/glog"
 	"github.com/gorilla/mux"
 	prom "github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"golang.org/x/crypto/bcrypt"
 
 	"github.com/pmorie/osb-broker-lib/pkg/rest"
 )
+
+type BasicAuth struct {
+	User string
+	Pass string
+}
+
+func (b *BasicAuth) Secret(user, realm string) string {
+	if user == b.User {
+		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(b.Pass), bcrypt.DefaultCost)
+		if err == nil {
+			return string(hashedPassword)
+		}
+	}
+	return ""
+}
 
 // Server is the server for the OSB REST API and the metrics API. A Server glues
 // the HTTP operations to their implementations.
@@ -27,14 +44,14 @@ type Server struct {
 }
 
 // New creates a new Router and registers all the necessary endpoints and handlers.
-func New(api *rest.APISurface, reg prom.Gatherer) *Server {
+func New(api *rest.APISurface, reg prom.Gatherer, enableBasicAuth bool, secret func(user, realm string) string) *Server {
 	router := mux.NewRouter()
 
 	if api.EnableCORS {
 		router.Methods("OPTIONS").HandlerFunc(api.OptionsHandler)
 	}
 
-	registerAPIHandlers(router, api)
+	registerAPIHandlers(router, api, enableBasicAuth, secret)
 	router.Handle("/metrics", promhttp.HandlerFor(reg, promhttp.HandlerOpts{}))
 
 	return &Server{
@@ -43,23 +60,32 @@ func New(api *rest.APISurface, reg prom.Gatherer) *Server {
 }
 
 // NewHTTPHandler creates a new Router and registers API handlers
-func NewHTTPHandler(api *rest.APISurface) http.Handler {
+func NewHTTPHandler(api *rest.APISurface, enableBasicAuth bool, secret func(user, realm string) string) http.Handler {
 	router := mux.NewRouter()
-	registerAPIHandlers(router, api)
+	registerAPIHandlers(router, api, enableBasicAuth, secret)
 	return router
 }
 
+func getHandleFunc(handler func(w http.ResponseWriter, r *http.Request), enableBasicAuth bool, secret func(user, realm string) string) func(w http.ResponseWriter, r *http.Request) {
+	if enableBasicAuth {
+		authenticator := auth.NewBasicAuthenticator("aws-service-broker", secret)
+		return auth.JustCheck(authenticator, handler)
+	} else {
+		return handler
+	}
+}
+
 // registerAPIHandlers registers the APISurface endpoints and handlers.
-func registerAPIHandlers(router *mux.Router, api *rest.APISurface) {
-	router.HandleFunc("/v2/catalog", api.GetCatalogHandler).Methods("GET")
-	router.HandleFunc("/v2/service_instances/{instance_id}/last_operation", api.LastOperationHandler).Methods("GET")
-	router.HandleFunc("/v2/service_instances/{instance_id}", api.ProvisionHandler).Methods("PUT")
-	router.HandleFunc("/v2/service_instances/{instance_id}", api.DeprovisionHandler).Methods("DELETE")
-	router.HandleFunc("/v2/service_instances/{instance_id}", api.UpdateHandler).Methods("PATCH")
-	router.HandleFunc("/v2/service_instances/{instance_id}/service_bindings/{binding_id}", api.BindHandler).Methods("PUT")
-	router.HandleFunc("/v2/service_instances/{instance_id}/service_bindings/{binding_id}", api.GetBindingHandler).Methods("GET")
-	router.HandleFunc("/v2/service_instances/{instance_id}/service_bindings/{binding_id}/last_operation", api.BindingLastOperationHandler).Methods("GET")
-	router.HandleFunc("/v2/service_instances/{instance_id}/service_bindings/{binding_id}", api.UnbindHandler).Methods("DELETE")
+func registerAPIHandlers(router *mux.Router, api *rest.APISurface, enableBasicAuth bool, secret func(user, realm string) string) {
+	router.HandleFunc("/v2/catalog", getHandleFunc(api.GetCatalogHandler, enableBasicAuth, secret)).Methods("GET")
+	router.HandleFunc("/v2/service_instances/{instance_id}/last_operation", getHandleFunc(api.LastOperationHandler, enableBasicAuth, secret)).Methods("GET")
+	router.HandleFunc("/v2/service_instances/{instance_id}", getHandleFunc(api.ProvisionHandler, enableBasicAuth, secret)).Methods("PUT")
+	router.HandleFunc("/v2/service_instances/{instance_id}", getHandleFunc(api.DeprovisionHandler, enableBasicAuth, secret)).Methods("DELETE")
+	router.HandleFunc("/v2/service_instances/{instance_id}", getHandleFunc(api.UpdateHandler, enableBasicAuth, secret)).Methods("PATCH")
+	router.HandleFunc("/v2/service_instances/{instance_id}/service_bindings/{binding_id}", getHandleFunc(api.BindHandler, enableBasicAuth, secret)).Methods("PUT")
+	router.HandleFunc("/v2/service_instances/{instance_id}/service_bindings/{binding_id}", getHandleFunc(api.GetBindingHandler, enableBasicAuth, secret)).Methods("GET")
+	router.HandleFunc("/v2/service_instances/{instance_id}/service_bindings/{binding_id}/last_operation", getHandleFunc(api.BindingLastOperationHandler, enableBasicAuth, secret)).Methods("GET")
+	router.HandleFunc("/v2/service_instances/{instance_id}/service_bindings/{binding_id}", getHandleFunc(api.UnbindHandler, enableBasicAuth, secret)).Methods("DELETE")
 	router.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("OK"))
 	})
